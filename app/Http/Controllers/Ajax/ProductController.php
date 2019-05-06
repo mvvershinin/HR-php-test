b0e0b740cdd858b4a71ddb1b335234a2ad5783b4
mvvershinin@gmail.com <mvvershinin@gmail.com>
2019-05-06 22:03:54 +0700

    order finished email

--- app/Http/Controllers/Ajax/ProductController.php
+++ app/Http/Controllers/Ajax/ProductController.php
@@ -13,48 +13,6 @@ define('CHANGE_PRICE_FORM', 'include.forms.price');
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\
@@ -88,16 +46,5 @@ class ProductController extends Controller
         ]);
 
         return response()->json( $product, 200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
     }
 }
